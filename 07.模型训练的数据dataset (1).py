6dee6c88c5792bf757eaf9e49bae7dfdc369e4bd
GarryWhite <3284263390@qq.com>
2025-11-06 17:24:45 +0800

    feat(dataset): 添加训练、测试和验证集的图像及标注文件
    添加了300张图像文件到dataset/images目录下的train、test和valid子目录
    同时添加了对应的标注文件到dataset/labels目录下的相应子目录

--- 07.模型训练的数据dataset (1).py
+++ 07.模型训练的数据dataset (1).py
@@ -12,7 +12,7 @@
 一般让
     训练数据集比例为85%,主要用于学习
     验证数据集比例为10%,用于调整优化
-    测试数据集比例为5%,用于最终检验
+    测试数据集比例为5%,用于最终检验1
 """
 import os
 # 创建目录
@@ -50,56 +50,33 @@ images_count = len(images)
 # print(images_count)
 
 # 计算各数据集的图片数量,按比例分配
-num_train = int(images_count * 0.85) # 训练数据集 85%
-num_valid = int(images_count * 0.1)  # 验证数据集 10%
+num_train = images_count * 0.85 # 训练数据集 85% 255
+num_valid = images_count * 0.1  # 验证数据集 10% 30
 num_test = images_count - num_train - num_valid # 剩下的就是测试数据集
 print(f"训练数据集有:{num_train}个,验证数据集有:{num_valid},测试数据集有:{num_test}")
 
-# 分割数据集
-# 训练集
-for i in range(num_train):
-    # 获取图片文件名
-    img_name = images[i]
-    # 获取对应的标签文件名
-    label_name = img_name.replace('.jpg', '.txt')
-    
-    # 复制图片到训练集
-    shutil.copy(f'./data/{img_name}', f'./dataset/images/train/{img_name}')
-    # 复制标签到训练集
-    shutil.copy(f'./data/{label_name}', f'./dataset/labels/train/{label_name}')
 
-# 验证集
-for i in range(num_train, num_train + num_valid):
-    # 获取图片文件名
-    img_name = images[i]
-    # 获取对应的标签文件名
-    label_name = img_name.replace('.jpg', '.txt')
-    
-    # 复制图片到验证集
-    shutil.copy(f'./data/{img_name}', f'./dataset/images/valid/{img_name}')
-    # 复制标签到验证集
-    shutil.copy(f'./data/{label_name}', f'./dataset/labels/valid/{label_name}')
+# 已经计算好了训练数据集,验证数据集,测试数据
+# 接下来,我们要拷贝数据
+count = 0 # 初始化的计数器
+for image in images:
+    if count < num_train: # 训练数据集
+        print("丢入训练数据集",count)
+        shutil.copy('./data/'+image,'./dataset/images/train')
+        # 复制对应文件的标签文件
+        # replace => 替换的语法 , 比如; img1.jpg 替换 img1.txt,然后将img1.txt存储到 dataset/labels/train
+        shutil.copy('./data/'+image.replace('jpg','txt'),'./dataset/labels/train')
+    elif count < num_valid + num_train:
+        print("丢入训练验证数据集", count)
+        shutil.copy('./data/'+image,'./dataset/images/valid')
+        # 对应的标签文件
+        shutil.copy('./data/'+image.replace('jpg','txt'),'./dataset/labels/valid')
+    else: # 测试数据集
+        print("丢入训练测试数据集", count)
+        shutil.copy('./data/'+image,'./dataset/images/test')
+        # 对应的标签文件
+        shutil.copy('./data/'+image.replace('jpg','txt'),'./dataset/labels/test')
 
-# 测试集
-for i in range(num_train + num_valid, images_count):
-    # 获取图片文件名
-    img_name = images[i]
-    # 获取对应的标签文件名
-    label_name = img_name.replace('.jpg', '.txt')
-    
-    # 复制图片到测试集
-    shutil.copy(f'./data/{img_name}', f'./dataset/images/test/{img_name}')
-    # 复制标签到测试集
-    shutil.copy(f'./data/{label_name}', f'./dataset/labels/test/{label_name}')
-
-# 复制classes.txt文件到dataset目录
-shutil.copy('./data/classes.txt', './dataset/classes.txt')
-
-print("数据集分割完成！")
-print(f"训练集: {num_train}张图片, 验证集: {num_valid}张图片, 测试集: {num_test}张图片")
-
-
-
-
-
+    print(f"{image}已经写入")
+    count += 1 # 每循环一次,计数器来累加一个1
 
